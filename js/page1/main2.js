2db22a738e446cb5f883a93190ca377be9055295
treinartz <thomas.reinartz@gmail.com>
2023-02-14 11:22:49 -0600

    Add files via upload

--- js/page1/main2.js
+++ js/page1/main2.js
@@ -1,7 +1,7 @@
 import CircleRunner from "./circleCircle/CircleRunner.js";
-import TileRunner from "./tile.js";
+import CirclesRunner from "./circleSmile/CirclesRunner.js";
 import CirclePointRunner from "./circlePoint/CirclePointRunner.js";
 
-new TileRunner(400, 400, "trigthree");
+new CirclesRunner(400, 400, "trigthree");
 new CircleRunner(400, 400, "trigone");
 new CirclePointRunner(400, 400, "trigtwo");
---
+++ js/page4/main.js
@@ -0,0 +1,5 @@
+import TileRunner from "./recur1/Tile.js";
+
+new TileRunner(400, 400, "recur1");
+new TileRunner(400, 400, "recur2");
+new TileRunner(400, 400, "recur3");
